009f048d4674582cd5507265c55c53d50a060f02
Paul Collyer <paul.collyer@grassvalley.com>
2024-01-20 14:10:30 +0000

    Added configurtation reader to the Test client

---
+++ tests/TestClient/GeneralConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace TestClient;
+
+/// <summary>
+/// Represents the general configuration.
+/// </summary>
+public class GeneralConfiguration
+{
+    /// <summary>
+    /// Gets or sets the URL.
+    /// </summary>
+    public string Url { get; set; }
+
+    /// <summary>
+    /// Gets or sets the API key.
+    /// </summary>
+    public string ApiKey { get; set; }
+}
--- tests/TestClient/Program.cs
+++ tests/TestClient/Program.cs
@@ -1,22 +1,34 @@
 ﻿namespace TestClient;
 
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
 using System.Text.Json;
 using System.Text.Json.Serialization.Metadata;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 using RestClient;
 
 public class Program
 {
     public static async Task Main(string[] args)
     {
-        var apiKey = "";
+        var appSettingsPath = GetAppSettingsPath(args);
+
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile(appSettingsPath, optional: true, reloadOnChange: false);
+
+        var config = builder.Build();
+
+        var generalConfig = new GeneralConfiguration();
+        config.GetSection("Startup:General").Bind(generalConfig);
 
         var context = new ClientContext();
         context.Options = new RestClientOptions()
         {
-            BaseUrl = "https://dev-eu-west-2-core.ampp.technology",
+            BaseUrl = generalConfig.Url,
             HttpClient = new HttpClient()
         };
 
@@ -28,7 +40,7 @@ public class Program
         ////    });
 
 
-        var tokenResult = await context.GetTokenClient().GetTokenAsync(apiKey);
+        var tokenResult = await context.GetTokenClient().GetTokenAsync(generalConfig.ApiKey);
 
 
         var json = JsonSerializer.Serialize<IServiceResult<string>>(
@@ -46,4 +58,35 @@ public class Program
         Console.WriteLine(result.Result?.Name);
     }
 
+    /// <summary>
+    /// Get location of appsettings.json file.
+    /// </summary>
+    /// <returns>Path to appsettings file.</returns>
+    /// <param name="args">The command line arguments.</param>
+    public static string GetAppSettingsPath(string[] args)
+    {
+        const string appSettingsKey = "appsettings";
+
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        if (string.IsNullOrWhiteSpace(environment) == false)
+        {
+            environment = "." + environment;
+        }
+
+        var settingFilesConfiguration = new ConfigurationBuilder()
+            .AddInMemoryCollection(
+                new Dictionary<string, string>
+                {
+                    { appSettingsKey, $"appsettings{environment}.json" },
+                })
+            .AddCommandLine(
+                args,
+                new Dictionary<string, string>
+                {
+                    { "--appsettings", appSettingsKey }
+                })
+            .Build();
+
+        return Path.Combine(AppContext.BaseDirectory, settingFilesConfiguration[appSettingsKey]);
+    }
 }
